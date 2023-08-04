882e551d5d24bcdf6e2417aa529597e242bb4f43
Kevin Garcia <kjesusg@gmail.com>
2023-08-03 20:28:22 -0600

    modifique texto

--- juego de texto.py
+++ juego de texto.py
@@ -59,24 +59,7 @@ def juego():
 
 if __name__ == "__main__":
     juego()
-
-# Videojuego de Texto de Recorrer Laberintos
-"""
-Este proyecto es un videojuego de texto en el que el jugador tiene que recorrer laberintos representados por caracteres ASCII. El objetivo es guiar al personaje (representado por la letra P) a través del laberinto, evitando las paredes (#) y alcanzando la meta (por ejemplo, un punto específico). El jugador puede mover al personaje utilizando las teclas ↑ ↓ ← → de su teclado.
-"""
-## Instrucciones
-"""
-1. Ejecuta el archivo `main.py` para comenzar el juego.
-2. Sigue las instrucciones en la consola para mover al personaje por el laberinto.
-3. Alcanza la meta para ganar el juego.
-
-¡Diviértete y buena suerte!
-
-Este proyecto forma parte de un curso de programación. Si tienes alguna sugerencia o mejora, no dudes en contribuir al repositorio.
-
----
-Realizado por [Tu Nombre]
-"""
+    
 if __name__ == "__main__":
     # Pedir el nombre del jugador por teclado
     nombre_jugador = input("KEVIN: ")
